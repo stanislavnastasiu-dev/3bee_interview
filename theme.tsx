b7aa2189277a76b4c0a4be50f4d88d275b99398d
stanislavnastasiu-dev <stanislavnastasiu0@gmail.com>
2022-03-25 12:20:37 +0100

    setup colori brand

---
+++ theme.tsx
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const colors = {
+  brand: {
+    900: '#ffb000',
+    800: '#ffd679',
+  },
+  secondary: '#f389a7',
+};
+
+export const theme = extendTheme({ colors });
