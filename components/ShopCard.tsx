ea92e6fc0f3a9ac90377f318b36b53823338f018
stanislavnastasiu-dev <stanislavnastasiu0@gmail.com>
2022-03-25 12:33:58 +0100

    definizione type per chiamata api

---
+++ components/ShopCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Card from './Card';
+
+interface ShopCardProps {}
+
+const ShopCard: React.FC<ShopCardProps> = ({}) => {
+  return <Card></Card>;
+};
+export default ShopCard;
